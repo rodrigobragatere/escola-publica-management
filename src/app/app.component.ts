47609380b76dfe331fbc4e9b7f993cc4f415c5a0
rodrigobragatere <75183495+rodrigobragatere@users.noreply.github.com>
2024-10-29 12:48:56 -0300

    sexto commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,37 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 import { Escola} from './models/escola';
 import { Turma} from './models/turma';
+import { FormsModule } from '@angular/forms'; // Importando FormsModule
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, RouterOutlet],
+  imports: [CommonModule, HttpClientModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'] // Atualiza para usar o arquivo SCSS
 })
 export class AppComponent implements OnInit {
   escolas: Escola[] = [];
   turmas: Turma[] = [];
+  novaEscola = { nome: '', endereco: '' };
+  novaTurma = { nome: '', serie: '',escolaId:0 };
 
   constructor(private http: HttpClient) {}
+
+  cadastrarEscola() {
+    if (this.novaEscola.nome && this.novaEscola.endereco) {
+      this.escolas.push({ ...this.novaEscola });
+      this.novaEscola = { nome: '', endereco: '' }; // Limpa o formulário
+    }
+  }
+
+  cadastrarTurma() {
+    if (this.novaTurma.nome && this.novaTurma.serie) {
+      this.turmas.push({ ...this.novaTurma });
+      this.novaTurma = { nome: '', serie: '',escolaId:0  }; // Limpa o formulário
+    }
+  }
 
   ngOnInit() {
     this.http.get<{ escolas: Escola[], turmas: Turma[] }>('/assets/dados.json')
