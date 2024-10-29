19b7c4efef5cf446b914aa488484de208c44cc81
rodrigobragatere <75183495+rodrigobragatere@users.noreply.github.com>
2024-10-29 13:50:19 -0300

    oitavo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,38 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
-import { Escola} from './models/escola';
-import { Turma} from './models/turma';
-import { FormsModule } from '@angular/forms'; // Importando FormsModule
-
+import { Escola } from './models/escola';
+import { Turma } from './models/turma';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, HttpClientModule, RouterOutlet, FormsModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'] // Atualiza para usar o arquivo SCSS
+  templateUrl: './app.component.html', // Verifique se este caminho está correto
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   escolas: Escola[] = [];
   turmas: Turma[] = [];
-  novaEscola = { id:'', nome: '', endereco: '' };
-  novaTurma = { nome: '', serie: '',escolaId:'' };
+  novaEscola = { id: '', nome: '', endereco: '' };
+  novaTurma = { id: '', nome: '', serie: '', escolaId: '' };
 
   constructor(private http: HttpClient) {}
 
   cadastrarEscola() {
     if (this.novaEscola.nome && this.novaEscola.endereco) {
       this.escolas.push({ ...this.novaEscola });
-      this.novaEscola = { id:'',nome: '', endereco: '' }; // Limpa o formulário
+      this.novaEscola = { id: '', nome: '', endereco: '' }; // Limpa o formulário
     }
   }
 
   cadastrarTurma() {
     if (this.novaTurma.nome && this.novaTurma.serie) {
       this.turmas.push({ ...this.novaTurma });
-      this.novaTurma = { nome: '', serie: '',escolaId:''  }; // Limpa o formulário
+      this.novaTurma = { id: '', nome: '', serie: '', escolaId: '' }; // Limpa o formulário
     }
+  }
+
+  excluirEscola(id: string) {
+    this.escolas = this.escolas.filter(escola => escola.id !== id);
+  }
+
+  excluirTurma(id: string) {
+    this.turmas = this.turmas.filter(turma => turma.id !== id);
   }
 
   ngOnInit() {
